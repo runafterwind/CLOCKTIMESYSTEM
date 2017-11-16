ffa2c92f30b7648b7ec9a5b53b882833d1a93d5d
lucky <769798754@qq.com>
2017-11-17 00:05:00 +0800

    add some api

--- clock/clocktim.c
+++ clock/clocktim.c
@@ -35,7 +35,7 @@
 char * Save_Templates(char * Key,char * Serial,char * fid,char *  Reseved)
 {
 
-#if 1
+#if 1		//发送部分
 	xmlDocPtr  doc = NULL;
        xmlNodePtr root_node = NULL;
 	xmlNodePtr tmpnode=NULL;
@@ -110,7 +110,8 @@ char * Save_Templates(char * Key,char * Serial,char * fid,char *  Reseved)
 		 tmpnode=tmpnode->next;
 	}
 
-	
+	xmlFreeDoc(doc);
+
 	
 	/*  将 conternt 发送给服务器端?*/
        return content;
@@ -120,7 +121,7 @@ FAILED:
      	}
 	  return NULL;
 
-#else
+#else		//接受部分
 		xmlDocPtr  doc = NULL;
 		xmlNodePtr root_node = NULL;
 		xmlNodePtr tmpnode=NULL;
@@ -147,7 +148,8 @@ FAILED:
 			
 		 tmpnode=tmpnode->next;
 	}
-		
+	xmlFreeDoc(doc);
+
 	return back;
 #endif
 
@@ -181,7 +183,7 @@ FAILED:
 
 */
 
-char * Update_Template_Status(char *Key,char * Serial ,char * fid,BOOL  ALL)
+int  Update_Template_Status(char *Key,char * Serial ,char * fid,BOOL  ALL)
 {
 
 
@@ -195,12 +197,76 @@ char * Update_Template_Status(char *Key,char * Serial ,char * fid,BOOL  ALL)
 	随后调用“Download_Templates_Batch_4”用于下载可用模板的批次。
 	key:
 	serial:
+
+	return:
+		<?xml version="1.0" encoding="UTF-8" ?>
+		<result>
+		 
+			<Batch>
+		 
+
+				<User>
+
+				</User>
+				<User>
+
+				</User>
+
+				<Finger_ID value="021890"/>
+
+
+				<Finger_ID value="000001"/>
+		 
+			</Batch>
+		</result>
+
 	
 */
 
-char *Download_Templates_4(char * Key,char * Serial) 
+int Download_Templates_4(char * Key,char * Serial) 
 {
 
+	/*发送？*/
+
+
+	/*接 	收*/
+	char * back;	//返回报文
+       int length;	//返回报文长度	
+	   
+	xmlDocPtr  doc = NULL;
+       xmlNodePtr root_node = NULL;
+	xmlNodePtr tmpnode=NULL;
+
+	char * fingerID[10]={NULL};		//最多支持10个
+	int count=0;
+	int i;
+	
+	doc=xmlReadMemory(back, length, NULL, NULL, 0);
+	root_node=xmlDocGetRootElement(doc);
+
+	tmpnode=root_node->children;
+	while(tmpnode)
+	{
+		if(xmlStrcmp(tmpnode->name, "Batch"))
+			{
+				tmpnode=tmpnode->children;
+				continue;
+			}
+
+		if(xmlStrcmp(tmpnode->name,"Finger_ID" ))
+			{
+			fingerID[count++]=xmlGetProp(tmpnode, "value");
+			}
+		
+		tmpnode=tmpnode->next;
+	}
+
+	/*处理 ?*/
+
+	for(i=0;i<10;i++)
+		if(fingerID[i])
+			free(fingerID[i]);
+		
 }
 
 
@@ -209,18 +275,14 @@ char *Download_Templates_4(char * Key,char * Serial)
 	key:
 	serial:
 	服务器的返回内容：
-	<?xml version="1.0" encoding="UTF-8" ?>
-	<result>
-	<current_time name="Account Current Time" value="2012-12-14 13:52:51" />
-	<utc_offset name="Account UTC Offset" value="-8.00" />
-	<account_name name="Account Name" value="Processing Point Test Room" />
-	</result>
+	
 
 */
-char *  Download_Templates_Batch_4(char *key,char *serial)
+int  Download_Templates_Batch_4(char *key,char *serial)
 {
 
 	char * back;	//服务器返回的数据，需要解析
+	
 	
 	
 }
@@ -232,14 +294,279 @@ char *  Download_Templates_Batch_4(char *key,char *serial)
 	key:
 	serial:
 
+	(success)
+	return:<?xml version="1.0" encoding="UTF-8" ?>
+	<result>
+	<current_time name="Account Current Time" value="2012-12-14 13:52:51" />
+	<utc_offset name="Account UTC Offset" value="-8.00" />
+	<account_name name="Account Name" value="Processing Point Test Room" />
+	</result>
+
+	(failed)
+	<?xml version="1.0" encoding="UTF-8" ?>
+	<result>
+	<current_time name="Sorry" value="Account not found" />
+	<utc_offset name="" value="" />
+	<account_name name="" value="" />
+	</result>
+
+
 */
-int update_time()
+int update_time(char * key,char * serial)
 {
 
+	/*发送报文? */
+
+	/*接收报文*/
+
+	char * back;	//返回报文
+       int length;	//返回报文长度	
+	   
+	xmlDocPtr  doc = NULL;
+       xmlNodePtr root_node = NULL;
+	xmlNodePtr tmpnode=NULL;
+	xmlAttrPtr tmpattr=NULL;
+	char*  curr_tim[2]={NULL};
+	char utc_offset[2]={NULL};
+	char account[2]={NULL};
+	
+	doc=xmlReadMemory(back, length, NULL, NULL, 0);
+	root_node=xmlDocGetRootElement(doc);
+
+	tmpnode=root_node->children;
+	while(tmpnode)
+	{
+		
+		if(!xmlStrcmp(tmpnode->name, (const char *)"current_time"))
+			{
+				//printf("%s value=%s \n",tmpnode->name,(fidattr=xmlGetNoNsProp(tmpnode, "value")));
+				curr_tim[0]=xmlGetNoNsProp(tmpnode, "name");
+				curr_tim]1]=xmlGetProp(tmpnode, "value");
+			}
+		if(!xmlStrcmp(tmpnode->name, (const char *)"utc_offset"))
+			{
+				//printf("%s value=%s \n",tmpnode->name,(resattr=xmlGetNoNsProp(tmpnode, "value")));
+				utc_offset[0]=xmlGetProp(tmpnode, "name");
+				utc_offset[1]=xmlGetProp(tmpnode, "value");
+			}
+
+		if(!xmlStrcmp(tmpnode->name, (const char *)"account_name"))
+			{
+				account[0]=xmlGetProp(tmpnode, "name");
+				account[1]=xmlGetProp(tmpnode, "value");
+			}
+		
+		tmpnode=tmpnode->next;
+	}
+
+	/*处理 ?*/
+
+	free(curr_tim[0]);
+	free(curr_tim[1]);
+	free(utc_offset[0]);
+	free(utc_offset[1]);
+	free(account[0]);
+	free(account[1]);
+	
+}
+
+
+
+/*
+	api1:向服务器发出更新logo请求，返回一个logo下载的URL
+	key:
+	serial:
+
+	返回报文：
+	<?xml version="1.0" encoding="UTF-8" ?>
+	<result>
+	<message name="Logo" value="http://ws.trackmytime.com/clock.cfc?method=get_logo&account=2162&size=3
+	20x240" />
+	</result>
+
+*/
+int update_logo(char * key,char * serial)
+{
+
+	/*  发送请求?*/
+	/*如何创建报文*/	
+	
+
+	/*接收返回*/
+	char * back;
+	int length;
+	xmlDocPtr  doc = NULL;
+       xmlNodePtr root_node = NULL;
+	xmlNodePtr tmpnode=NULL;
+
+	char* name=NULL;
+	char* URL=NULL;
+
+	doc=xmlReadMemory(back, length, NULL, NULL, 0);
+	root_node=xmlDocGetRootElement(doc);
+	tmpnode=root_node->children;
+	if(xmlStrcmp(tmpnode, "message"))
+		{
+			name=xmlGetNoNsProp(tmpnode, "name");
+			URL=xmlGetNoNsProp(tmpnode, "value");
+		}
+
+	/*处理 ?*/
+	
+	free(URL);
+	free(name);
+	xmlFreeDoc(doc);
 
 }
 
 
+
+
+/*
+	Key
+ 		AJ5P8EG3M2Z3
+	Serial
+		e.g., BN4000-20082123
+	Diagnostic_Message
+		e.g., "Camera lens failure"
+
+	return:
+	<?xml version="1.0" encoding="UTF-8" ?>
+	<result>
+	<message value ="Request received. Ticket has been created." />
+	</result>
+
+
+*/
+
+int Diagnostic_Alert(char *key ,char * serial,char * diagnostc_msg)
+{
+
+	/*创建报文*/
+	
+	xmlDocPtr  doc = NULL;
+       xmlNodePtr root_node = NULL;
+	xmlNodePtr tmpnode=NULL;
+	xmlAttrPtr tmpattr=NULL;
+	
+	doc = xmlNewDoc(BAD_CAST"1.0");
+	if (doc == NULL) {
+     		fprintf(stderr, "Failed to new doc.\n");
+     		return -1;
+     	}
+	
+		 //创建根节点
+	 root_node = xmlNewNode(NULL, BAD_CAST"result");		
+	 if (root_node == NULL) {
+		fprintf(stderr, "Failed to new root node.\n");
+		return -1;
+	 }
+	xmlDocSetRootElement(doc, root_node);
+	tmpnode=xmlNewChild(root_node, NULL,BAD_CAST"messag", NULL);	
+	xmlNewProp(tmpnode,"value",(xmlChar * )diagnostc_msg);
+	xmlSaveFormatFileEnc(PATH, doc, "UTF-8", 1);
+	xmlFreeDoc(doc);
+	
+	/*报文解析*/
+	char * back;
+	int length;
+	xmlDocPtr  doc = NULL;
+       xmlNodePtr root_node = NULL;
+	xmlNodePtr tmpnode=NULL;
+	char * msg=NULL;
+	
+	doc=xmlReadMemory(back, length, NULL, NULL, 0);
+	root_node=xmlDocGetRootElement(doc);
+	tmpnode=root_node->children;
+	if(xmlStrcmp(tmpnode, "message"))
+	{
+		msg=xmlGetNoNsProp(tmpnode, "value");
+	}
+	/*处理 ?*/
+
+	free(msg);
+	xmlFreeDoc(doc);
+}
+
+
+/*
+	api2:先服务器请求更新固件，服务器返回固件下载的URL
+	key:
+	serial:
+	version:
+
+	服务器返回:
+	<?xml version="1.0" encoding="UTF-8" ?>
+	<result>
+		<firmware>
+			<current_fwvrs>BN4000-4.9</current_fwvrs>
+			<current_fwurl>http://ws.trackmytime.com/firmware/BN3000-4.9.bin</current_fwurl>
+			<current_fwsize>12551168</current_fwsize>
+		</firmware>
+	</result>
+
+*/
+int Update_Firmware2(char *key,char * setial,char * version)
+{
+	/*创建报文?*/
+
+
+	/*解析报文*/
+	char * back;
+	int length;
+	xmlDocPtr  doc = NULL;
+       xmlNodePtr root_node = NULL;
+	xmlNodePtr tmpnode=NULL;
+
+	xmlChar * fwvrs=NULL,*fwurl=NULL,*fwsize;
+
+	doc=xmlReadMemory(back, length, NULL, NULL, 0);
+	root_node=xmlDocGetRootElement(doc);
+	tmpnode=root_node->children;
+	while()
+	{
+		if(xmlStrcmp(tmpnode->name, "firmware"))
+			{
+				tmpnode=tmpnode->children;
+				continue;
+			}
+
+		if(xmlStrcmp(tmpnode->name, "current_fwvrs"))
+			{
+				fwvrs=xmlNodeGetContent(tmpnode);
+			}
+		if(xmlStrcmp(tmpnode->name, "current_fwurl"))
+			{
+				fwurl=xmlNodeGetContent(tmpnode);
+			}
+		if(xmlStrcmp(tmpnode->name, "current_fwsize"))
+			{
+				fwsize=xmlNodeGetContent(tmpnode);
+			}
+
+		tmpnode=tmpnode->next;
+		
+	}
+
+
+	/*处理*/
+
+	free(fwsize);
+	free(fwurl);
+	free(fwvrs);
+}
+
+
+/*
+	api39:用于按地区获取支持电话号码
+	
+	
+*/
+
+int Get_Support_Number()
+{
+
+}
 
 
 
